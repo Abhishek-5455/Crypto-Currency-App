865d312763a94be1f7a93f053384867ecd33e57d
Abhishek Rathod <abhishekrathod5455@gmail.com>
2023-07-25 15:49:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.cryptoapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptoapp.presentation.coin_detail.components.CoinDetailScreen
+import com.example.cryptoapp.presentation.coin_list.components.CoinListScreen
+import com.example.cryptoapp.ui.theme.CryptoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ){
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ){
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ){
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
